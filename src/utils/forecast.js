2d4b067d9a0eab9ec1b587294e3904a8f8fae07e
unknown <hrachya.petros@gmail.com>
2025-02-01 12:50:32 +0400

    Add High and Low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,25 @@
 const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=6882d9633993d0db1f912aeae141cb6b&query=' + latitude + ',' + longitude + '&units=f';
+    const url = `http://api.weatherstack.com/current?access_key=6882d9633993d0db1f912aeae141cb6b&query=${latitude},${longitude}&units=f`;
 
-    request({url, json: true}, (error, {body}) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
-            callback('Unable to connect to weather service.', undefined);
+            callback("Unable to connect to weather service.", undefined);
         } else if (body.error) {
-            callback('Unable to find location.', undefined);
+            callback("Unable to find location.", undefined);
         } else {
-            console.log(body.current);
             const temperature = body.current.temperature;
             const feelslike = body.current.feelslike;
             const description = body.current.weather_descriptions[0];
             const precipChance = body.current.precip;
 
-            callback(undefined, `${description}. It is currently ${temperature}°F out. There is a ${precipChance}% chance of rain.`);
+            callback(
+                undefined,
+                `${description}. It is currently ${temperature}°F out. It feels like ${feelslike}°F. There is a ${precipChance}% chance of rain.`
+            );
         }
     });
 };
 
-
 module.exports = forecast;
-
